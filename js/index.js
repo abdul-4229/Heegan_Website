0f8c03241845fbe142c3cf47dbf0fef675d1f76b
gaay4229 <mosharafhilwa@gmail.com>
2023-09-05 22:35:37 +0300

    success

---
+++ js/index.js
@@ -0,0 +1,24 @@
+
+function sendMail() {
+    var params = {
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        message: document.getElementById("message").value,
+        
+    };
+    
+    const serviceID = "service_b4n427r";
+    const templateID = "template_u42046r";
+  
+      emailjs.send(serviceID, templateID, params)
+      .then(res=>{
+          document.getElementById("name").value = "";
+          document.getElementById("email").value = "";
+          document.getElementById("message").value = "";
+          console.log(res);
+          alert("Your message sent successfully!!")
+  
+      })
+      .catch(err=>console.log(err));
+  
+  }
